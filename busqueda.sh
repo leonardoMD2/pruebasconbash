aa5a30adb7ab4cfe473607d4776dcc26ce0b7d7c
Leonardo <martinezjleonardo@gmail.com>
2023-01-23 20:46:31 -0300

    Versión 1: el script genera una busqueda en todo el escritorio utilizando la extensión del archivo como argumento

---
+++ busqueda.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ #script de busqueda según extensión
+
+
+echo 'Script de busqueda según el argumento'
+echo 'Por defecto se buscará en escritorio.'
+find /c/Users/Leonardo/Desktop/ -name *$1 | less
+
