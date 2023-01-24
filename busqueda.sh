445688c25ba0d7b0edecafa14f9d5d3e87397ea7
Leonardo <martinezjleonardo@gmail.com>
2023-01-24 11:29:01 -0300

    Se comentó el código para mejor explicación de los comandos bash; se cambió el nombre de la función de busqueda

--- busqueda.sh
+++ busqueda.sh
@@ -2,14 +2,18 @@
  #script de busqueda según extensión
 
 # modularizando
-main (){
+busqueda (){
     echo 'Script de busqueda según el argumento'
     echo 'Por defecto se buscará en escritorio.'
     find /c/Users/Leonardo/Desktop/ -name "*$1" > busqueda.txt
+    #find busca en el path y la flag -name le indica el nombre del archivo, nosotros pasamos como argumento la extensión ingresada 
+    #luego lo guarda en el archivo busqueda.txt
     mv busqueda.txt /c/Users/Leonardo/Desktop/
+    #en esta parte lo que hacemos es mover el archivo con las rutas a escritorio
     echo 'Se agregaron las rutas al archivo'
 }
-main $1
+busqueda $1
+#mandamos a llamar la función y le pasamos el argumento
 
 
 
