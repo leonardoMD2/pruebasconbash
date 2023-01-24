2a267bf431486d1a6060df1f9e4f1f3bf8be1a20
Leonardo <martinezjleonardo@gmail.com>
2023-01-23 21:23:27 -0300

    Se cambió la ruta en la que se guarda el archivo de busqueda al escritorio y se acomodó el código solucionando el error que el archivo quedaba vacio

--- busqueda.sh
+++ busqueda.sh
@@ -4,7 +4,8 @@
 
 echo 'Script de busqueda según el argumento'
 echo 'Por defecto se buscará en escritorio.'
-find /c/Users/Leonardo/Desktop/ -name *$1 > busqueda.txt
+find /c/Users/Leonardo/Desktop/ -name "*$1" > busqueda.txt
+mv busqueda.txt /c/Users/Leonardo/Desktop/
 echo 'Se agregaron las rutas al archivo'
 
 
