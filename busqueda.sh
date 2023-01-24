c13186366d49c08522bb3ba1966bc405fe7e113e
Leonardo <martinezjleonardo@gmail.com>
2023-01-23 23:02:26 -0300

    Modularizando el script en experimental

--- busqueda.sh
+++ busqueda.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
  #script de busqueda según extensión
 
+# modularizando
+main (){
+    echo 'Script de busqueda según el argumento'
+    echo 'Por defecto se buscará en escritorio.'
+    find /c/Users/Leonardo/Desktop/ -name "*$1" > busqueda.txt
+    mv busqueda.txt /c/Users/Leonardo/Desktop/
+    echo 'Se agregaron las rutas al archivo'
+}
+main $1
 
-echo 'Script de busqueda según el argumento'
-echo 'Por defecto se buscará en escritorio.'
-find /c/Users/Leonardo/Desktop/ -name "*$1" > busqueda.txt
-mv busqueda.txt /c/Users/Leonardo/Desktop/
-echo 'Se agregaron las rutas al archivo'
+
 
 
