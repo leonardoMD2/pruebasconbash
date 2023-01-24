8b17fe6f5306c9a656547528815e79c7f653c684
Leonardo <martinezjleonardo@gmail.com>
2023-01-23 21:15:10 -0300

    Se agregó la posibilidad de que el usuario pase el argumento de la extensión a buscar y se guardan las rutas en el archivo adjunto

--- busqueda.sh
+++ busqueda.sh
@@ -4,5 +4,7 @@
 
 echo 'Script de busqueda según el argumento'
 echo 'Por defecto se buscará en escritorio.'
-find /c/Users/Leonardo/Desktop/ -name *$1 | less
+find /c/Users/Leonardo/Desktop/ -name *$1 > busqueda.txt
+echo 'Se agregaron las rutas al archivo'
+
 
