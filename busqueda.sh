d1042ef1e51db53ccde95a58e2b254345438bb77
Leonardo <martinezjleonardo@gmail.com>
2023-01-24 12:44:40 -0300

    Se acomodó el ingreso de la extensión y ahora sólo se necesita escribirla sin el punto

--- busqueda.sh
+++ busqueda.sh
@@ -5,7 +5,7 @@
 busqueda (){
     echo 'Script de busqueda según el argumento'
     echo 'Por defecto se buscará en escritorio.'
-    find /c/Users/Leonardo/Desktop/ -name "*$1" > busqueda.txt
+    find /c/Users/Leonardo/Desktop/ -name "*.$1" > busqueda.txt
     #find busca en el path y la flag -name le indica el nombre del archivo, nosotros pasamos como argumento la extensión ingresada 
     #luego lo guarda en el archivo busqueda.txt
     mv busqueda.txt /c/Users/Leonardo/Desktop/
@@ -14,7 +14,7 @@ busqueda (){
 }
 busqueda $1
 #mandamos a llamar la función y le pasamos el argumento
-echo 'desde github'
+
 
 
 
